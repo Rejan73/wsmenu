b80789c5efb6ad7d5dc9816a1e2432f2f19f7a7d
rodolphe-deparis <rodolphe.deparis@wordline.com>
2020-07-17 10:57:05 +0200

    WS Menu

---
+++ src/main/java/rod/menu/WSMenuApplication.java
@@ -0,0 +1,24 @@
+package rod.menu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSMenuApplication implements CommandLineRunner
+{
+
+  public static void main(final String[] args)
+  {
+    SpringApplication.run(WSMenuApplication.class, args);
+
+  }
+
+  @Override
+  public void run(final String... args) throws Exception
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
