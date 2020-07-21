156ec8c3084718b1ac742833fec8ab29f937b4e0
rodolphe-deparis <rodolphe.deparis@wordline.com>
2020-07-21 16:46:44 +0200

    Partie Web

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -3,8 +3,9 @@ function findMenu(id){
 	 $.ajax({
 		 url: "/menus/"+id
 	 }).then(function(data){
-		 $('.greeting-id').append(data.id);
-	 $('.greeting-content').append(data.nom);
+		 var msg="";
+		 data.plats.forEach(element => msg+="<li class=\"list-group-item\">"+element.nom+"</li>");
+		 document.getElementById("detail").innerHTML=data.nom+"<br><ul class=\"list-group\">"+msg+"</ul>";
 	 });
 }
 
@@ -13,7 +14,7 @@ function findAllMenu(){
 		 url: "/menus"
 	 }).then(function(data){
 		 var msg="";
-		 data.forEach(element => msg+="<li class=\"list-group-item\">"+element.id +":"+ element.nom+"</li>");
+		 data.forEach(element => msg+="<li class=\"list-group-item\" onclick=\"findMenu("+element.id+")\">"+ element.nom+"</li>");
 		 document.getElementById("resume" ).innerHTML="<ul class=\"list-group\">"+msg+"</ul>";
 	 });	
 	
