7b7d5c8507b94363b27174138fd71442fabf206f
rodolphe-deparis <rodolphe.deparis@wordline.com>
2020-07-28 17:37:31 +0200

    menu form

--- src/main/java/rod/menu/enums/TypeRepas.java
+++ src/main/java/rod/menu/enums/TypeRepas.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TypeRepas {
-	  entree("entre"),
+	  entree("entree"),
 	  plat("plat"),
 	  dessert("dessert");
 		
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -15,7 +15,7 @@ function findAllMenu(){
 	 }).then(function(data){
 		 var msg="";
 		 data.forEach(element => msg+="<li class=\"list-group-item\" onclick=\"findMenu("+element.id+")\">"+ element.nom+"</li>");
-		 document.getElementById("resume" ).innerHTML="<ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("resume" ).innerHTML="Liste des Menus : <br/><ul class=\"list-group\">"+msg+"</ul>";
 		 document.getElementById("detail").innerHTML="";
 	 });	
 	
@@ -27,7 +27,7 @@ function findAllPlat(){
 	 }).then(function(data){
 		 var msg="";
 		 data.forEach(element => msg+="<li class=\"list-group-item\" onclick=\"findPlat("+element.id+")\">"+ element.nom+"</li>");
-		 document.getElementById("resume" ).innerHTML="<ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("resume" ).innerHTML="Liste des Plats : <br/><ul class=\"list-group\">"+msg+"</ul>";
 		 document.getElementById("detail").innerHTML="";
 	 });
 }
@@ -38,17 +38,50 @@ function findPlat(id){
 	 }).then(function(data){
 		 var msg="";
 		 data.ingredients.forEach(element => msg+="<li class=\"list-group-item\">"+element.nom+" : "+element.quantite+" "+element.typeMesure+"</li>");
-		 document.getElementById("detail" ).innerHTML="Détail "+data.nom+"<a href=\"#\" onclick=\"showAddIngredient("+data.id+")\">+</a><br><ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("detail" ).innerHTML="Détail "+data.nom+"<a href=\"#\" onclick=\"showFormIngredient("+data.id+")\">+</a><br><ul class=\"list-group\">"+msg+"</ul>";
 	 });
 }
-function showAddIngredient(id){
+function showFormIngredient(id){
 	document.getElementById("platid").value=id;
 	document.getElementById("formIngredient").style.visibility = "visible";
 }
 
+function putIngredient(){
+	dataToSend="{ \"nom\": \""+document.getElementById("nomIngredient").value+"\",  \"quantite\":"+ document.getElementById("quantite").value+",  \"typeMesure\":\""+document.getElementById("typeMesure").value+"\"}"
+	$.ajax({
+	    type: "PUT",
+	    url: "/plats/"+document.getElementById("platid").value,
+	    contentType: "application/json",
+	    data: dataToSend,
+	success: function(data) {
+		document.getElementById("formIngredient").style.visibility = "hidden";
+		findPlat(document.getElementById("platid").value)
+	  }
+	});
+	
+}
+
+function showFormPlat(){
+	document.getElementById("formPlat").style.visibility = "visible";
+}
+
+function postPlat(){
+	dataToSend="{ \"nom\": \""+document.getElementById("nomPlat").value+"\",  \"nbPersonne\":"+ document.getElementById("nbPersonne").value+",  \"typerepas\":\""+document.getElementById("typeRepas").value+"\"}"
+	$.ajax({
+	    type: "POST",
+	    url: "/plats/",
+	    contentType: "application/json",
+	    data: dataToSend,
+	success: function(data) {
+		hideAll();
+		findAllPlat();
+	  }
+	});
+	
+}
 
 function hideAll() {
-  
+  document.getElementById("formPlat").style.visibility = "hidden";
   document.getElementById("formIngredient").style.visibility = "hidden";
 }	
 
