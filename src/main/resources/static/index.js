f49f3003d22f6be170fd03cf88d4181c1ca64d78
rodolphe-deparis <rodolphe.deparis@wordline.com>
2020-07-28 11:31:48 +0200

    ajout form ingredient

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,7 +5,7 @@ function findMenu(id){
 	 }).then(function(data){
 		 var msg="";
 		 data.plats.forEach(element => msg+="<li class=\"list-group-item\">"+element.nom+"</li>");
-		 document.getElementById("detail").innerHTML=data.nom+"<br><ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("detail").innerHTML="Détail "+data.nom+"<br><ul class=\"list-group\">"+msg+"</ul>";
 	 });
 }
 
@@ -16,6 +16,7 @@ function findAllMenu(){
 		 var msg="";
 		 data.forEach(element => msg+="<li class=\"list-group-item\" onclick=\"findMenu("+element.id+")\">"+ element.nom+"</li>");
 		 document.getElementById("resume" ).innerHTML="<ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("detail").innerHTML="";
 	 });	
 	
 }
@@ -27,6 +28,7 @@ function findAllPlat(){
 		 var msg="";
 		 data.forEach(element => msg+="<li class=\"list-group-item\" onclick=\"findPlat("+element.id+")\">"+ element.nom+"</li>");
 		 document.getElementById("resume" ).innerHTML="<ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("detail").innerHTML="";
 	 });
 }
 
@@ -36,6 +38,17 @@ function findPlat(id){
 	 }).then(function(data){
 		 var msg="";
 		 data.ingredients.forEach(element => msg+="<li class=\"list-group-item\">"+element.nom+" : "+element.quantite+" "+element.typeMesure+"</li>");
-		 document.getElementById("detail" ).innerHTML=data.nom+"<br><ul class=\"list-group\">"+msg+"</ul>";
+		 document.getElementById("detail" ).innerHTML="Détail "+data.nom+"<a href=\"#\" onclick=\"showAddIngredient("+data.id+")\">+</a><br><ul class=\"list-group\">"+msg+"</ul>";
 	 });
 }
+function showAddIngredient(id){
+	document.getElementById("platid").value=id;
+	document.getElementById("formIngredient").style.visibility = "visible";
+}
+
+
+function hideAll() {
+  
+  document.getElementById("formIngredient").style.visibility = "hidden";
+}	
+
